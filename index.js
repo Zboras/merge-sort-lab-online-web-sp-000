3925afc2a9e949b49d355ecb94db3b62b14131ed
Zach Boras <zrboras@gmail.com>
2020-12-19 04:17:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-function findMinAndRemoveSorted(firstHalf, secondHalf){
-  let minfirstHalf = firstHalf[0]
-  let minsecondHalf = secondHalf[0]
- 
-  if(minfirstHalf < minsecondHalf){
-    return firstHalf.shift()
-  } else {
-    return secondHalf.shift()
+function findMinAndRemoveSorted(array) {
+  let min = array[0];
+  let minIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    }
   }
+  array.splice(minIndex,1);
+  return min;
 }
-
 
   function merge(firstHalf, secondHalf){
     let sorted = []
