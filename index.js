96d009fab1326ec70f7eb8df8a95291acda10523
Zach Boras <zrboras@gmail.com>
2020-12-19 04:58:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,4 +12,13 @@ function findMinAndRemoveSorted(array) {
 }
 
  
+ function findMinAndRemove(firstHalf, secondHalf){
+  let minfirstHalf = firstHalf[0]
+  let minsecondHalf = secondHalf[0]
  
+  if(minfirstHalf < minsecondHalf){
+    return firstHalf.shift()
+  } else {
+    return secondHalf.shift()
+  }
+}
