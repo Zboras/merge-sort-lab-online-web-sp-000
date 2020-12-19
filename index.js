109294182c961399a2e1079cfa28630377de92b2
Zach Boras <zrboras@gmail.com>
2020-12-19 04:50:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ function findMinAndRemoveSorted(array) {
 
   function merge(firstHalf, secondHalf){
     let sorted = []
+    function findMinAndRemove(firstHalf, secondHalf){
+  let minfirstHalf = firstHalf[0]
+  let minsecondHalf = secondHalf[0]
+ 
+  if(minfirstHalf < minsecondHalf){
+    return firstHalf.shift()
+  } else {
+    return secondHalf.shift()
+  }
+}
     while(firstHalf.length != 0 && secondHalf.length != 0){
       let currentMin = findMinAndRemove(firstHalf, secondHalf)
       sorted.push(currentMin)
